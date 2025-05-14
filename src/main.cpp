802718415d6a9976c33b6be6e9ef40e2bdea59f1
Prathik Narsetty <prathiknarsetty@gmail.com>
2025-05-14 03:06:52 -0500

    average BPM seems to be working well. Use this for data buffer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-// Prathik Narsetty
+// Prathik narsetty
+
 /*
   Hardware Connections (Breakoutboard to Arduino):
   -5V = 5V (3.3V is allowed)
@@ -14,18 +15,24 @@
 #include <Wire.h>
 #include "MAX30105.h"
 
+#include "heartRate.h"
+
 MAX30105 particleSensor;
+
+const byte RATE_SIZE = 8; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
 
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
-  while (!Serial);
+    while (!Serial);
   Serial.println("Initializing...");
 Wire.begin(21, 22); // SDA = GPIO21, SCL = GPIO22
-
-  // Initialize sensor
-
 
 Serial.println("I2C Scanner");
   for (byte addr = 1; addr < 127; addr++) {
@@ -38,48 +45,55 @@ Serial.println("I2C Scanner");
   }
   Serial.println("Done");
           delay(1000);  
- // 
+
+          
+  // Initialize sensor
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
     Serial.println("MAX30105 was not found. Please check wiring/power. ");
-    while (1) {
-      // also for debugging
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(1000);                      // wait for a second
-        digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-        delay(1000);  
-    };
+    while (1);
   }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
 
-  //Setup to sense a nice looking saw tooth on the plotter
-  byte ledBrightness = 0x1F; //Options: 0=Off to 255=50mA
-  byte sampleAverage = 32; //Options: 1, 2, 4, 8, 16, 32
-  byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  int sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200 
-  int pulseWidth = 411; //Options: 69, 118, 215, 411 (411 for more accuracy)
-  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
-
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
-
-  //Arduino plotter auto-scales annoyingly. To get around this, pre-populate
-  //the plotter with 500 of an average reading from the sensor
-
-  //Take an average of IR readings at power up
-  const byte avgAmount = 64;
-  long baseValue = 0;
-  for (byte x = 0 ; x < avgAmount ; x++)
-  {
-    baseValue += particleSensor.getIR(); //Read the IR value
-  }
-  baseValue /= avgAmount;
-
-  //Pre-populate the plotter so that the Y scale is close to IR values
-  for (int x = 0 ; x < 500 ; x++)
-    Serial.println(baseValue);
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 }
 
 void loop()
 {
-  
-  Serial.println(particleSensor.getIR()); //Send raw data to plotter
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+
+  Serial.print("IR=");
+  Serial.print(irValue);
+  Serial.print(", BPM=");
+  Serial.print(beatsPerMinute);
+  Serial.print(", Avg BPM=");
+  Serial.print(beatAvg);
+
+  if (irValue < 50000)
+    Serial.print(" No finger?");
+
+  Serial.println();
 }
